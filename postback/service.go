a708fe8dacca6981a988de02d5f532634478c3d6
e.netesov <evgeny@netesov.org>
2026-06-16 13:44:25 +0300

    update postback service interface

--- postback/service.go
+++ postback/service.go
@@ -1,14 +1,13 @@
 package postback
 
 import (
-	ver2 "github.com/CLi-Ter/binomv2-postback"
 	bpClient "github.com/CLi-Ter/binomv2-postback/client"
 )
 
 // PostbackService предназначен для обслуживания постбеков и отправки их в правильный трекер.
 type Service interface {
 	// SendEvent - обновляет клик данными о событиях.
-	SendEvent(clickID string, ev ver2.Event) error
+	SendEvent(clickID string, ev bpClient.Event) error
 	// SendPostback - отправляет постбек по клику.
 	// Создает конверсию или обновляет клик в зависимости от содержания запроса.
 	SendPostback(bpClient.Request) error
